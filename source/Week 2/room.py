893845e6655594506b4465dcd515683330127f43
caitlingbailey <64934329+caitlingbailey@users.noreply.github.com>
2021-06-01 22:27:38 +0100

    Update room.py

--- source/Week 2/room.py
+++ source/Week 2/room.py
@@ -1,4 +1,13 @@
 class Room():
+    '''
+    This is a docstring
+    '''
     def __init__(self, room_name):
         self.name = room_name
         self.description = None
+
+    def set_description(self, room_description):
+        self.description = room_description
+    
+    def get_description(self):
+        return self.description
