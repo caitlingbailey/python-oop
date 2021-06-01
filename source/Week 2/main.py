785b58b6240f082de1c066a7e28922f276db4467
caitlingbailey <64934329+caitlingbailey@users.noreply.github.com>
2021-06-01 22:34:31 +0100

    Add getters and setters

---
+++ source/Week 2/main.py
@@ -0,0 +1,8 @@
+from room import Room
+
+kitchen = Room("kitchen")
+ballroom = Room("ballroom")
+dining_hall = Room("dining hall")
+
+kitchen.set_description("A dank and dirty room buzzing with flies")
+print( kitchen.description )
--- source/Week 2/room.py
+++ source/Week 2/room.py
@@ -5,9 +5,18 @@ class Room():
     def __init__(self, room_name):
         self.name = room_name
         self.description = None
+    
+    def set_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
 
     def set_description(self, room_description):
         self.description = room_description
     
     def get_description(self):
         return self.description
+    
+    def describe(self):
+        print( self.description )
