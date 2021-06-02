851716237bfb4e01348dd2865785b254d9265065
caitlingbailey <64934329+caitlingbailey@users.noreply.github.com>
2021-06-02 10:28:44 +0100

    Completed Week 2

--- source/Week 2/main.py
+++ source/Week 2/main.py
@@ -1,8 +1,26 @@
 from room import Room
+from item import Item
 
 kitchen = Room("kitchen")
-ballroom = Room("ballroom")
-dining_hall = Room("dining hall")
-
 kitchen.set_description("A dank and dirty room buzzing with flies")
-print( kitchen.description )
+
+ballroom = Room("ballroom")
+ballroom.set_description("A vast room with a shiny wooden floor; huge candlesticks guard the entrance")
+
+dining_hall = Room("dining hall")
+dining_hall.set_description("A large room with ornate golden decorations on each wall")
+
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom, "west")
+ballroom.link_room(dining_hall, "east")
+
+current_room = kitchen
+
+sword = Item("sword")
+print(sword.get_name())
+# while True:
+#     print("\n")
+#     current_room.get_details()
+#     command = input("Which way? ")
+#     current_room = current_room.move(command)
--- source/Week 2/room.py
+++ source/Week 2/room.py
@@ -5,6 +5,7 @@ class Room():
     def __init__(self, room_name):
         self.name = room_name
         self.description = None
+        self.linked_rooms = {}
     
     def set_name(self, name):
         self.name = name
@@ -20,3 +21,23 @@ class Room():
     
     def describe(self):
         print( self.description )
+
+    def link_room(self, room_to_link, direction):
+        self.linked_rooms[direction] = room_to_link
+        # print( self.name + " linked rooms :" + repr(self.linked_rooms) )
+
+    def get_details(self):
+        print(f"The {self.name}")
+        print("---------------")
+        print(f"{self.description}")
+        for direction in self.linked_rooms:
+            room = self.linked_rooms[direction]
+            print(f"The {room.get_name()} is {direction}")
+        print("\n")
+
+    def move(self, direction):
+        if direction in self.linked_rooms:
+            return self.linked_rooms[direction]
+        else:
+            print("You can't go that way")
+            return self
