4d853bdc90bba76dc205effd4c5b3830bc114d3e
caitlingbailey <64934329+caitlingbailey@users.noreply.github.com>
2021-06-03 11:20:23 +0100

    Update setup.py

--- source/setup.py
+++ source/setup.py
@@ -4,10 +4,14 @@ __project__ = "rpg_game"
 __version__ = "0.0.1"
 __description__ = "A text-based RPG game"
 __packages__ = ["rpg_game"]
+__author__ = "Caitlin Bailey"
+__author_email__ = "caitlinbailey9@gmail.com"
 
 setup(
     name = __project__,
     version = __version__,
     description = __description__,
     packages = __packages__,
+    author = __author__,
+    author_email = __author_email__,
 )
